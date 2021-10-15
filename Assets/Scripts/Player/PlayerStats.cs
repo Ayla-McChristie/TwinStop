cd5ab3337c323d6c2362b0dee667282202f7bf49
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2021-10-14 22:32:44 -0500

    fixes door prefab

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -3,56 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-    [RequireComponent(typeof(PlayerMovement))]
-    class PlayerStats : MonoBehaviour
+[RequireComponent(typeof(PlayerMovement))]
+class PlayerStats : MonoBehaviour
+{
+    public int health; // Total amount of health left
+    public int numOfHearts; // Max amount of hearts a player can have, should be 3
+
+    public int keys;
+    public int bossKeys;
+
+    public Image[] hearts; // all heart UI game objects go here
+    public Sprite fullHeart; // sprite of full heart here
+    public Sprite emptyHeart; // sprite of empty heart here
+
+    void Update()
     {
-        public int health; // Total amount of health left
-        public int numOfHearts; // Max amount of hearts a player can have, should be 3
-
-        public int numOfKeys;
-
-        public Image[] hearts; // all heart UI game objects go here
-        public Sprite fullHeart; // sprite of full heart here
-        public Sprite emptyHeart; // sprite of empty heart here
-
-        void Update()
+        // Health Lock
+        if (health > numOfHearts)
         {
-            // Health Lock
-            if (health > numOfHearts)
-            {
-                health = numOfHearts; // this makes sure that players can never go over the set amount of hearts
+            health = numOfHearts; // this makes sure that players can never go over the set amount of hearts
 
-            }
-
-            // system for turning full hearts to empty hearts
-            for (int i = 0; i < hearts.Length; i++)
-            {
-                if (i < health)
-                {
-                    hearts[i].sprite = fullHeart;
-                }
-                else
-                {
-                    hearts[i].sprite = emptyHeart;
-                }
-
-                if (i < numOfHearts)
-                {
-                    hearts[i].enabled = true;
-                }
-                else
-                {
-                    hearts[i].enabled = false;
-                }
-            }
         }
 
-        // TODO add code for projectiles
-        void OnCollisionEnter(Collision other)
+        // system for turning full hearts to empty hearts
+        for (int i = 0; i < hearts.Length; i++)
         {
-            if (other.gameObject.tag == "Enemy") // || other.gameObject.tag == "Projectiles"?
+            if (i < health)
             {
-                health--;
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
             }
         }
     }
+
+    // TODO add code for projectiles
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy") // || other.gameObject.tag == "Projectiles"?
+        {
+            health--;
+        }
+    }
+}
