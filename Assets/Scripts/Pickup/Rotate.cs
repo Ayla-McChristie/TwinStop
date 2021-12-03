4ea272a89578b357639bc149cc50f1a450e597b7
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2021-12-03 01:21:45 -0600

    adds spinning trap

--- Assets/Scripts/Pickup/Rotate.cs
+++ Assets/Scripts/Pickup/Rotate.cs
@@ -6,10 +6,19 @@ public class Rotate : MonoBehaviour
 {
     [SerializeField]
     Vector3 rotationSpeed;
+    [SerializeField]
+    bool isTimeBased;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.Rotate(rotationSpeed);
+        if (isTimeBased)
+        {
+            this.transform.Rotate(rotationSpeed * Time.timeScale);
+        }
+        else
+        {
+            this.transform.Rotate(rotationSpeed);
+        }
     }
 }
---
+++ Assets/Scripts/Room/SpinningWallTrap.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rotate))]
+public class SpinningWallTrap : MonoBehaviour
+{
+    CapsuleCollider capsule;
+    // Start is called before the first frame update
+    void Start()
+    {
+        capsule = this.GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeManager.Instance != null)
+        {
+            Debug.Log(TimeManager.Instance.isTimeStopped.ToString());
+            if (TimeManager.Instance.isTimeStopped)
+            {
+                capsule.enabled = false;
+            }
+            else
+            {
+                capsule.enabled = true;
+            }
+        }
+    }
+}
