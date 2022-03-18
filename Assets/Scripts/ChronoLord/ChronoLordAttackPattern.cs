e3ce2e78d8d7b23a072f47d73beed8adf8c4a01e
Ryan <44453329+rhruocco@users.noreply.github.com>
2022-03-17 19:55:56 -0500

    climax work

--- Assets/Scripts/ChronoLord/ChronoLordAttackPattern.cs
+++ Assets/Scripts/ChronoLord/ChronoLordAttackPattern.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ChronoLordAttackPattern : MonoBehaviour
 {
+    Animator MyAnimator;
+
+    [SerializeField]
+    GameObject[] Chargers;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MyAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsWaveFinished(Chargers))
+        {
+            MyAnimator.SetTrigger("ChargerWaveEnd");
+        }
+    }
+
+    bool IsWaveFinished(GameObject[] enemiesInWave)
+    {
+        foreach (GameObject enemy in enemiesInWave)
+        {
+            if (enemy.activeInHierarchy)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
