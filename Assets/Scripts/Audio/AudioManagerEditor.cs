28accd1ec99f54e63029d936cf8fcbde7acbaa98
JustinH-94 <61295115+JustinH-94@users.noreply.github.com>
2022-03-25 10:01:46 -0500

    Update on Some of the Rooms

--- Assets/Scripts/Audio/AudioManagerEditor.cs
+++ Assets/Scripts/Audio/AudioManagerEditor.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+#if UNITY_EDITOR
 [CustomEditor(typeof(AudioManager))]
 public class AudioManagerEditor : Editor
 {
-    public override void OnInspectorGUI()
+    public new void OnInspectorGUI()
     {
         DrawDefaultInspector();
 
         AudioManager aM = (AudioManager)target;
-
         if (GUILayout.Button("Adjust Volume"))
             aM.AdjustFromInspector();
     }
 }
+#endif
