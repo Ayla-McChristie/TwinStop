e132c50411cefbbeee5f7bcfb21fe51855465381
Ryan <ruoccoryan@gmail.com>
2022-04-11 21:34:22 -0500

    added bridge button cool down

--- Assets/Scripts/ShootableButton.cs
+++ Assets/Scripts/ShootableButton.cs
@@ -11,33 +11,51 @@ public class ShootableButton : MonoBehaviour
     //bool that sets whether the button can be shot multiple times
     [SerializeField]
     bool OnlyPressableOnce;
+
+    float CooldownInt, TimerIncrement;
     // Start is called before the first frame update
     void Start()
     {
         MyBridgeScript = MyBridge.GetComponent<Bridge>();
+        TimerIncrement = .1f;
+        CooldownInt = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CooldownInt > 0)
+        {
+            Timer();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("PlayerBullet"))
         {
-            if (MyBridgeScript.amIUp)
+            if (CooldownInt <= 0)
             {
-                Debug.Log("put down");
-                MyBridgeScript.PutBridgeDown();
+                if (MyBridgeScript.amIUp)
+                {
+                    Debug.Log("put down");
+                    MyBridgeScript.PutBridgeDown();
+                }
+                else
+                {
+                    Debug.Log("put up");
+                    MyBridgeScript.PutBridgeUp();
+                }
+                CooldownInt = 10;
             }
-            else
-            {
-                Debug.Log("put up");
-                MyBridgeScript.PutBridgeUp();
-            }
-            
         }
+    }
+
+    /// <summary>
+    /// Activated after shooting the button. Increments the CooldownInt var
+    /// </summary>
+    void Timer()
+    {
+        CooldownInt -= TimerIncrement;
     }
 }
